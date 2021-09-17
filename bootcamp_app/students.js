ae73dc462a6c411647221fc82017c74a40a50722
Sam Fay <sam.fay.1984@gmail.com>
2021-09-17 03:57:35 +0000

    Parameterized Query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,8 +7,6 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-//Get the value from the terminal
-
 const queryString = `
   SELECT students.id as student_id, students.name as name, cohorts.name as cohort
   FROM students
@@ -17,13 +15,12 @@ const queryString = `
   LIMIT $2;
   `;
 
-  pool.query(`
-  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-  FROM students
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  LIMIT ${process.argv[3] || 5};
-  `)
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
