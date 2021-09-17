8279e0aec45238c3f5af02cf4e9c870d29cb67d6
Sam Fay <sam.fay.1984@gmail.com>
2021-09-17 03:14:40 +0000

    Querying in Bootcampx database

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,12 +7,28 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+//Get the value from the terminal
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
 pool.query(`
-SELECT id, name, cohort_id
-FROM students
-LIMIT 5;
-`)
-.then(res => {
-  console.log(res,rows);
-})
-.catch(err => console.error('query error', err.stack));
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  LIMIT 5;
+  `)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
+  });
